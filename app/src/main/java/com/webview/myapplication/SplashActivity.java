97766dccfc5bbd915c903da43169759dc866a4b2
avidipta-hazra <57026736+avidipta-hazra@users.noreply.github.com>
2026-01-19 20:42:06 +0530

    Update SplashActivity.java

--- app/src/main/java/com/webview/myapplication/SplashActivity.java
+++ app/src/main/java/com/webview/myapplication/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,14 +19,23 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        typingText = findViewById(R.id.typingText);
+        // STEP 3 STARTS HERE 👇
+        View logo = findViewById(R.id.logo);
+        logo.animate()
+                .alpha(1f)
+                .translationYBy(-40)
+                .setDuration(1000)
+                .setStartDelay(300)
+                .start();
+        // STEP 3 ENDS HERE 👆
 
+        typingText = findViewById(R.id.typingText);
         startTypingEffect();
 
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 3000); // total splash time
+        }, 3000);
     }
 
     private void startTypingEffect() {
